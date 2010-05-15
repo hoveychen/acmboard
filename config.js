d8961076e6e5e3c01938def46b849ad559cdc1b7
hoveychen <hoveychen@747a9ed9-4d07-9722-040c-6d157726ebf5>
2010-05-15 15:44:14 +0000

    Import original version, used in GDCPC2010

---
+++ config.js
@@ -0,0 +1,13 @@
+var boardTitle = "Guangdong College Programming Contest 2010";
+var teamHeight = 50;
+var cssYes = "ui-state-active";
+var cssPending = "ui-state-highlight";
+var cssNo = "ui-state-error";
+var cssEmpty = "ui-state-disabled";
+var currentBoardFileUrl = "gdcpc2010/current.html";
+var finalBoardFileUrl = "gdcpc2010/final.html";
+var startDelay = 2000;
+var jumpDelay = 2000;
+var openProblemDelay = 1000;
+var tickPerInterval = 10;
+var scrollPosition = 8;
